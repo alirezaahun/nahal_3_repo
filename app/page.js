54b136e4839b6deccf63467c06d8fb0313a1604a
alireza <alirezaahoun@gmail.com>
2025-03-15 04:33:26 -0700

    filter layout

--- app/page.js
+++ app/page.js
@@ -11,19 +11,38 @@ const getPosts = async () => {
 
 
 export default async function Home() {
-  
+
   const projects = await getPosts();
- 
+
   return (
 
     <div className="container px-4 py-8 mx-auto">
-    <h1 className="mb-6 text-3xl font-bold">لیست پروژه ها</h1>
-    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-      {projects.map((project, index) => (
-        <Project key={index} project={project} />
-      ))}
+      <h1 className="mb-6 text-3xl font-bold">لیست پروژه ها</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+          placeholder="جست و جو"
+        />
+      </div>
+
+      <div className="mb-4">
+        <select
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg"
+        >
+          <option value="All">بدون فیلتر</option>
+          <option value="notStarted">شروع نشده</option>
+          <option value="InProgress">درحال انجام</option>
+          <option value="deliverd">تحویل شده</option>
+          <option value="confirmed">تایید شده</option>
+        </select>
+      </div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project, index) => (
+          <Project key={index} project={project} />
+        ))}
+      </div>
     </div>
-  </div>
     // <>
     //   {
     //     posts.map(item => (
--- components/Project.js
+++ components/Project.js
@@ -26,17 +26,21 @@ export default function Project({ project, isFavorite = false }) {
     }, [fave])
 
     return (
-        <Link href={`/Project/${project.id}`}>
-            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
-                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{project.name}</h2>
-                <p className="mb-4 font-normal text-gray-700">{project.description}</p>
-                <div className="text-xl mb-2">
-                    <span style={{ cursor: 'pointer' }} onClick={() => changeValue(project.id)} className={fave.includes(project.id) ? 'text-yellow-500' : 'text-gray-500 '}>★</span>
-                </div>
-                <p className="text-sm text-gray-500">تاریخ تحویل: {project.delivery_date}</p>
 
+        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md">
+            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{project.name}</h2>
+            <p className="mb-4 font-normal text-gray-700">{project.description}</p>
+            <div className="text-xl mb-2">
+                <span style={{ cursor: 'pointer' }} onClick={() => changeValue(project.id)} className={fave.includes(project.id) ? 'text-yellow-500' : 'text-gray-500 '}>★</span>
             </div>
-        </Link>
+            <p className="text-sm text-gray-500">تاریخ تحویل: {project.delivery_date}</p>
+            <div className="mt-4">
+                <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href={`/Project/${project.id}`}>
+                    جزئیات پروژه
+                </Link>
+            </div>
+        </div>
+
 
     )
 }
